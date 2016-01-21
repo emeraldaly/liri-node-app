4c418a6697cc0a961729a23fa224d2402c3ba615
Alyson <emerald.aly@gmail.com>
2016-01-20 21:49:03 -0500

    returns values, but all undefined

--- movieThis.js
+++ movieThis.js
@@ -1,7 +1,16 @@
+//module.exports = function(){
 // request code
-request('http://www.omdbapi.com/?t=frozen', function (error, response, body) {
-  parsedBody = JSON.parse(response.body);
+
+
+if(movie === undefined) {
+  movie = "mr. nobody";
+}
+var request = require('request');
+var movie = process.argv.slice[3];
+
+request('http://www.omdbapi.com/?t=' + movie + '&y=&plot=short&r=json&tomatoes=true', function (error, response, body) {
   if (!error && response.statusCode == 200) {
+    var movieBack = JSON.parse(response.body);
     console.log("The title is", body.Title);
     console.log("The year it released is", body.Year);
     console.log("The OMDB rating is", body.imdbRating);
@@ -9,12 +18,11 @@ request('http://www.omdbapi.com/?t=frozen', function (error, response, body) {
     console.log("The movie is in this language:", body.Language);
     console.log("The plot is", body.Plot);
     console.log("These are some of the actors in the movie:", body.Actors);
-    console.log("The Rotten Tomatoes Rating is", body.);
-    console.log("See more Rotten Tomato info at this link", body.);
-      if(err) {
-        return console.log(err); 
+    console.log("The Rotten Tomatoes Rating is", body.tomatoRating);
+    console.log("See more Rotten Tomato info at this link", body.tomatoURL);
   }
-})
+});
+//}
 
 //movie
  /* request("http://www.omdbapi.com/?t=frozen&y=&plot=short&r=json", function(error, response, body) {
